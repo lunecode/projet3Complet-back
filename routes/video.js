a6d42379e033c83870cf410855b828c2781d9ccb
Yous <ysouane0@gmail.com>
2019-05-29 19:55:40 +0200

    WIP road

--- routes/video.js
+++ routes/video.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+connection = require('../conf');
 
 router.get('/', (req, res) => {
     res.status(200).send('Je suis dans /video')
