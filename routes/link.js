ced097e1eeaa902d8d121b250d409613978ccfe2
Yous <ysouane0@gmail.com>
2019-06-03 17:13:56 +0200

    cleaning merge

--- routes/link.js
+++ routes/link.js
@@ -1,15 +1,289 @@
 const express = require('express');
+const connection = require('../conf');
 const router = express.Router();
-connection = require('../conf');
+
 
 router.get('/', (req, res) => {
-    res.status(200).send('Je suis dans /link')
+    res.status(200).send('Je suis dans link')
+});
+
+
+//******************************************* */ CRUD  table de liaison profil / change history profil/*********************************************************************************** */
+// récupération des données
+
+router.get('/dataprofilchangehistoryprofil', (req, res) => {
+    connection.query('SELECT * FROM profil_has_change_history_profil ', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// Insertion des donneés
+
+
+router.post('/insertprofilchangehistoryprofil', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO profil_has_change_history_profil SET ?', formData, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de la sauvegarde des datas");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+//suppression des données
+router.delete('/deleteprofilchangehistoryprofil/:id', (req, res) => {
+    const dropChangeHistoryProfil = req.params.id;
+    connection.query('DELETE FROM profil_has_change_history_profil WHERE profil_id_profil = ?', [dropChangeHistoryProfil], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression des données");
+        } else {
+            res.sendStatus(200);
+        }
+    });
 });
 
 
 
 
-// **************************************************CRUD de la table travel_step_has_change_history_travel_step***************************************************************************
+//*************************************** */ CRUD  table de liaison watch later with profil*********************************************************************************** */
+
+
+
+// récupération des données
+router.get('/datawatch_later_has_profil', (req, res) => {
+    connection.query('SELECT * FROM watch_later_has_profil', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// Insertion des donneés
+
+router.post('/insertwatch_later_has_profil', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO watch_later_has_profil SET ?', formData, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de la sauvegarde des datas");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+//suppression des données
+
+router.delete('/deletewatch_later_has_profil/:id', (req, res) => {
+    const dropWatchlaterHasProfil = req.params.id;
+    connection.query('DELETE FROM watch_later_has_profil WHERE watch_later_id_watch_later = ?', [dropWatchlaterHasProfil], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression des données");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+
+
+
+/* *********************CRUD table liaison profil/popularity**********************************/
+
+
+router.get('/profil_has_popularity', (req, res) => {
+    connection.query('SELECT * from profil_has_popularity', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération de la table des liaisons');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+
+
+router.post('/profil_has_popularity', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO profil_has_popularity SET ?', formData, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de l'envoi du message");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+router.delete('/profil_has_popularity/:id', (req, res) => {
+    const dropprofil_has_popularity = req.params.id;
+    connection.query('DELETE FROM profil_has_popularity WHERE profil_id_profil = ?', [dropprofil_has_popularity], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression d'une ");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+
+/* *********************CRUD table liaison profil/changehistoryvideo**********************************/
+
+
+
+router.get('/profil_has_change_history_video', (req, res) => {
+    connection.query('SELECT * from profil_has_change_history_video', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération de la table des liaisons');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+
+
+
+router.post('/profil_has_change_history_video', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO profil_has_change_history_video SET ?', formData, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de l'envoi de la table des liaisons");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+
+router.delete('/profil_has_change_history_video/:id', (req, res) => {
+    const dropprofil_has_change_history_video = req.params.id;
+    connection.query('DELETE FROM profil_has_change_history_video WHERE profil_id_profil = ?', [dropprofil_has_change_history_video], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression d'une ");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+//**************************** */Table de liaison profil - follower*****************************************
+
+
+// requete get profil - follower
+router.get('/getprofilfollower', (req, res) => {
+    // connection to the database, and selection of employees
+    connection.query('SELECT * FROM profil_has_follower ', (err, results) => {
+        if (err) {
+            // If an error has occurred, then the user is informed of the error
+            res.status(500).send('Erreur lors de la récupération des données');
+        } else {
+            // If everything went well, we send the result of the SQL query as JSON.
+            res.json(results);
+        }
+    });
+});
+
+// requete post profil - follower 
+
+router.post('/postprofilfollower', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO profil_has_follower SET ?', formData, (err, results) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send("Erreur lors de la sauvegarde d'un employé");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+//request delete profil_has_follower
+router.delete('/deleteprofilfollower', (req, res) => {
+    const dropProfilFollower = req.params.id;
+    connection.query('DELETE FROM profil_has_follower WHERE profil_id_profil = ?', [dropProfilFollower], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression des données");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+
+
+// -------------------------***PROFIL PLAYLIST***-------------------------
+// ---------Affichage des données de la table profil - playlist-----------
+
+router.get('/profilplaylist', (req, res) => {
+    connection.query('SELECT * FROM profil_has_playlist', (err, results) => {
+        if (err) {
+            res.status(500).send('Erreur de la récupération des données');
+        } else {
+            res.json(results);
+        };
+    });
+});
+
+
+
+// -----------Insérer les données de la table profil - playlist------------
+
+router.post('/profilplaylist', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO profil_has_playlist SET ?', formData, (err, results) => {
+        if (err) {
+            res.status(500).send("Erreur lors de l'envoi du message");
+        }
+        else {
+            res.sendStatus(200);
+        }
+    })
+});
+
+// ----------Supprimer les données de la table profil - playlist-----------
+
+router.delete('/profilplaylist/:id', (req, res) => {
+    const dropprofil_has_playlist = req.params.id;
+    connection.query('DELETE FROM profil_has_playlist WHERE profil_id_profil = ?', [dropprofil_has_playlist], err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Erreur lors de la suppression d'une playlist");
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+
+
+
+
+// **************************CRUD de la table travel_step_has_change_history_travel_step***************************************************************************
 
 
 // Affichage de toutes les données de la table travel_step_has_change_history_travel_step ( testé )
@@ -19,9 +293,11 @@ router.get('/getdatafkchangehistorytravelstep', (req, res) => {
             res.status(500).send('Erreur lors de la récupération de la table change history travel step')
         } else {
             res.json(results)
-        }
-    })
-})
+        };
+    });
+});
+
+
 
 // Route pour insérer des données via un POST ( testé )
 router.post('/insertdatafkchangehistorytravelstep', (req, res) => {
@@ -32,8 +308,8 @@ router.post('/insertdatafkchangehistorytravelstep', (req, res) => {
         } else {
             res.sendStatus(200)
         }
-    })
-})
+    });
+});
 
 
 // Route pour supprimer une données via DELETE ( testé )
@@ -46,8 +322,8 @@ router.delete('/deletedatafkchangehistorytravelstep/:id', (req, res) => {
         } else {
             res.sendStatus(200)
         }
-    })
-})
+    });
+});
 
 
 
@@ -63,8 +339,8 @@ router.get('/getdatafkcomment_has_change_history_like_comment', (req, res) => {
         } else {
             res.json(results)
         }
-    })
-})
+    });
+});
 
 // Route pour insérer des données via un POST ( testé )
 router.post('/insertdatafkcomment_has_change_history_like_comment', (req, res) => {
@@ -75,8 +351,8 @@ router.post('/insertdatafkcomment_has_change_history_like_comment', (req, res) =
         } else {
             res.sendStatus(200)
         }
-    })
-})
+    });
+});
 
 
 // Route pour supprimer une données via DELETE ( testé )
@@ -89,8 +365,8 @@ router.delete('/deletedatafkcomment_has_change_history_like_comment/:id', (req, 
         } else {
             res.sendStatus(200)
         }
-    })
-})
+    });
+});
 
 
 
