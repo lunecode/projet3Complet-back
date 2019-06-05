0881587e97b1f821e10651d22db593fcf30fecf9
Yous <ysouane0@gmail.com>
2019-06-05 09:56:56 +0200

    WIP

--- routes/profil.js
+++ routes/profil.js
@@ -106,4 +106,201 @@ router.delete('/delete_general_video/:id', (req, res) => {
 });
 
 
+
+
+
+
+
+
+
+// ***************************
+
+
+
+// listen to the url “/notification”
+router.get('/getnotification', (req, res) => {
+  // connection to the database, and selection of employees
+  connection.query('SELECT * FROM notification ', (err, results) => {
+    if (err) {
+      // If an error has occurred, then the user is informed of the error
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      // If everything went well, we send the result of the SQL query as JSON.
+      res.json(results);
+    }
+  });
+});
+
+//request put notification
+router.put('/putnotification/:id', (req, res) => {
+
+  const idNotification = req.params.id;
+  const formData = req.body;
+
+  connection.query('UPDATE notification SET ? WHERE id_notification = ?', [formData, idNotification], err => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la modification d'une notification");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+});
+
+// request post notificaiton
+router.post('/postnotification', (req, res) => {
+  const formData = req.body;
+  connection.query('INSERT INTO notification SET ?', formData, (err, results) => {
+
+    if (err) {
+      console.log(err)
+      res.status(500).send("Erreur lors de la sauvegarde d'un employé");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+//request delete notification
+router.delete('/deletenotification', (req, res) => {
+
+  const dropNotification = req.params.id;
+
+  connection.query('DELETE FROM notification WHERE id_notification = ?', [dropNotification], err => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la suppression des notifications");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+});
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<
+//  request get follower 
+
+router.get('/getfollower', (req, res) => {
+
+  connection.query('SELECT * FROM follower ', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+
+      res.json(results);
+    }
+
+
+  });
+});
+
+//request put follower
+
+router.put('/putfollower/:id', (req, res) => {
+  const idFollower = req.params.id;
+  const formData = req.body;
+
+  connection.query('UPDATE follower SET ? WHERE id_follower = ?', [formData, idFollower], err => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la modification d'un follower");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+});
+
+// request post follower
+router.post('/postfollower', (req, res) => {
+
+  const formData = req.body;
+
+  connection.query('INSERT INTO follower SET ?', formData, (err, results) => {
+
+    if (err) {
+      console.log(err)
+      res.status(500).send("Erreur lors de la sauvegarde d'un employé");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+//request delete follower
+router.delete('/deletefollower', (req, res) => {
+  const dropFollower = req.params.id;
+  connection.query('DELETE FROM follower WHERE id_follower= ?', [dropFollower], err => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la suppression d'un follower");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<
+//  require get following
+
+router.get('/getfollowing', (req, res) => {
+
+  connection.query('SELECT * FROM following ', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+
+      res.json(results);
+    }
+  });
+});
+
+//request put following
+router.put('/putfollowing/:id', (req, res) => {
+  const idFollowing = req.params.id;
+  const formData = req.body;
+
+  connection.query('UPDATE following SET ? WHERE id_following = ?', [formData, idFollowing], err => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la modification d'un following");
+    } else {
+
+      res.sendStatus(200);
+    }
+  });
+});
+
+// request post following
+router.post('/postfollowing', (req, res) => {
+
+  const formData = req.body;
+
+  connection.query('INSERT INTO following SET ?', formData, (err, results) => {
+
+    if (err) {
+      console.log(err)
+      res.status(500).send("Erreur lors de la sauvegarde d'un following");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+//request delete following
+router.delete('/deletefollowing', (req, res) => {
+  const dropFollowing = req.params.id;
+  connection.query('DELETE FROM following WHERE id_following= ?', [dropFollowing], err => {
+
+    if (err) {
+      console.log(err);
+      res.status(500).send("Erreur lors de la suppression d'un following");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
 module.exports = router;
