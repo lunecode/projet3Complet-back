1920d1fa60ef571bde13235e0832df9a49c2f491
Yous <ysouane0@gmail.com>
2019-07-24 22:37:32 +0200

    add Readme.md

--- Readme.md
+++ Readme.md
@@ -1,7 +1,17 @@
-#Tripitto Project
+#Tripitto Project Back-API
 
+This repository manages the back end part of the project
 
-Clone the project and execute "npm i" in the terminal for install dependancy
-Import the database SQL and think to change de password SQL in "conf.js" and user account mysql and password in "database/db.js" files of the project
-The SQL file and diagram is in "database_Schema_SQL" directory
-For start the back execute "npm start" in the terminal, start the back first and the front after
+List of applications used : Mysql Workbench, NodeJS, Express, Mysql, bcrypt, body-parser, cors, jsonwebtoken, morgan, mysql2, nodemon, sequelize
+
+##**Process to follow :** 
+
+* 1 - Clone the Github project and execute "npm i" in the terminal for install dependancy
+
+* 2 - Import the database SQL structure empty or with data with the file in /database_Schema_SQL folder 
+
+* 3 - Change in the file /conf.js and /database/db.js the user and password sql
+
+* 4 - Install dependency with the commande line " npm i " in the terminal
+
+* 4 - For start the back end use " npm start" in the terminal, you need to start the back first and the front after
--- routes/general_video.js
+++ routes/general_video.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.status(200).send('Je suis dans /general_video')
+  res.status(200).send('Je suis dans /general_video')
 });
 
 
@@ -14,12 +14,12 @@ router.get('/', (req, res) => {
 
 router.get('/get_general_video', (req, res) => {
   connection.query('SELECT * FROM general_video', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des données');
-      } else {
-        res.json(results);
-      }
-    });
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 // Insertion des donneés
@@ -53,7 +53,7 @@ router.put('/update_general_video/:id', (req, res) => {
 
 router.delete('/delete_general_video/:id', (req, res) => {
   const dropGeneralVideo = req.params.id;
-  connection.query('DELETE  FROM general_video WHERE profil_id_profil = ?', [dropGeneralVideo], err => {
+  connection.query('DELETE FROM general_video WHERE id_general_video = ?', [dropGeneralVideo], err => {
     if (err) {
       console.log(err);
       res.status(500).send("Erreur lors de la suppression des données");
@@ -66,25 +66,25 @@ router.delete('/delete_general_video/:id', (req, res) => {
 
 router.get('/get_general_video_liked_popularitylimit', (req, res) => {
   connection.query('SELECT * FROM general_video INNER JOIN liked ON id_general_video=liked.general_video_id_general_video INNER JOIN popularity ON id_popularity=liked.popularity_id_popularity LIMIT 4', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des données');
-      }else{
-        res.json(results);
-      }
-    });
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 router.get('/get_profil_limite1/:offset', (req, res) => {
   const offset = +req.params.offset
   connection.query('SELECT * FROM profil LIMIT ? , 1;', [offset], (err, results) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Erreur lors de la récupération des données');
-      }else{
-        res.json(results);
-      }
-    });
- });
+    if (err) {
+      console.log(err);
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
+});
 
 //************************ */JOINTURE /********************************************************** */
 
@@ -92,12 +92,12 @@ router.get('/get_profil_limite1/:offset', (req, res) => {
 
 router.get('/get_general_video_liked_popularity', (req, res) => {
   connection.query('SELECT * FROM general_video INNER JOIN liked ON id_general_video=liked.general_video_id_general_video INNER JOIN popularity ON id_popularity=liked.popularity_id_popularity', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des données');
-      }else{
-        res.json(results);
-      }
-    });
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 
@@ -108,12 +108,12 @@ router.get('/get_general_video_liked_popularity', (req, res) => {
 router.get('/get_general_video_limite/:offset', (req, res) => {
   const offset = +req.params.offset
   connection.query('SELECT * FROM general_video LIMIT ? , 5;', [offset], (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des données');
-      }else{
-        res.json(results);
-      }
-    });
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 //******* NEXT DESTINATION ******* */
@@ -128,16 +128,16 @@ router.get('/get_general_video_nextdestination', (req, res) => {
 });
 
 
-// TEST GET VIDEO FOR HOME PAGE WITH COUNTRY
+// GET VIDEO FOR HOME PAGE WITH COUNTRY
 
 router.get('/get_general_video_home', (req, res) => {
   connection.query('SELECT * from general_video JOIN travel_information ON id_general_video=travel_information.general_video_id_general_video LIMIT 5', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des données');
-      } else {
-        res.json(results);
-      }
-    });
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 
@@ -146,12 +146,12 @@ router.get('/get_general_video_home', (req, res) => {
 
 router.get('/get_id_general_video', (req, res) => {
   connection.query('SELECT id_general_video FROM general_video ORDER BY id_general_video DESC', (err, results) => {
-      if (err) {
-        res.status(500).send('Erreur lors de la récupération des données');
-      } else {
-        res.json(results);
-      }
-    });
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 
@@ -172,7 +172,6 @@ router.get('/get_general_video_nextdestination', (req, res) => {
 // ***********************************GET VIDEO WITH ID PROFIL******************************************
 
 
-// TEST OK WITH POSTMAN
 router.get('/get_video_id_profil/:id', (req, res) => {
   const getVideoProfil = req.params.id
   connection.query('SELECT * FROM general_video WHERE profil_id_profil = ?', [getVideoProfil], (err, results) => {
@@ -185,7 +184,6 @@ router.get('/get_video_id_profil/:id', (req, res) => {
 })
 
 
-// TEST
 router.get('/get_general_video_travel_information/:id', (req, res) => {
   const getVideoByProfil = req.params.id
   connection.query('SELECT * from general_video JOIN travel_information ON id_general_video=travel_information.general_video_id_general_video WHERE profil_id_profil = ?', [getVideoByProfil], (err, results) => {
@@ -214,27 +212,27 @@ router.get('/get_general_video_nextdestination_tauxderemplissage', (req, res) =>
 router.get('/get_general_video_lim/:offset', (req, res) => {
   const offset = +req.params.offset
   connection.query('SELECT * FROM general_video LIMIT ? , 4;', [offset], (err, results) => {
-      if (err) {
-        console.log(err);
-        
-        res.status(500).send('Erreur lors de la récupération des données');
-      }else{
-        res.json(results);
-      }
-    });
+    if (err) {
+      console.log(err);
+
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 router.get('/get_Page_Afrique/:offset', (req, res) => {
   const offset = +req.params.offset
   connection.query('SELECT * FROM general_video LIMIT ? , 8;', [offset], (err, results) => {
-      if (err) {
-        console.log(err);
-        
-        res.status(500).send('Erreur lors de la récupération des données');
-      }else{
-        res.json(results);
-      }
-    });
+    if (err) {
+      console.log(err);
+
+      res.status(500).send('Erreur lors de la récupération des données');
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 
